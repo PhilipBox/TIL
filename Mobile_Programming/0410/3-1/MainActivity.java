872e95bffb3c538265bd03a470ae9d80ac1bf65d
unknown <jihwanla17@naver.com>
2018-04-10 23:13:20 +0900

    0410_Mobile Programming Study

---
+++ Mobile_Programming/0410/3-1/MainActivity.java
@@ -0,0 +1,40 @@
+package org.androidtown.hello;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = (LinearLayout) findViewById(R.id.container);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+
+                CheckBox checkBox = (CheckBox) container.findViewById(R.id.checkBox);
+                checkBox.setText("로딩되었습니다.");
+            }
+        });
+
+    }
+}
