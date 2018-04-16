a2ab174d5527a67dd8c500fdef004fc87610b1d9
unknown <jihwanla17@naver.com>
2018-04-16 16:09:16 +0900

    0416 Moible Programming

---
+++ Mobile_Programming/0416/Fragment2/Java/MainActivity.java
@@ -0,0 +1,20 @@
+package org.androidtown.myapp0416;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }//end onCreate
+
+
+
+}//end Class
